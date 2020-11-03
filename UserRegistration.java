7749b50907d4682c40a1f7823ef518cf914066dc
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-11-03 19:48:05 +0530

    Validation for LastName

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,15 +6,31 @@ public class UserRegistration {
 	@Test
 	public void checkingFirstname() {
 		UserValidator validator = new UserValidator();
-		boolean isFirstName = validator.checkFirstname("Rekha");
-		Assert.assertTrue(isFirstName);
+		boolean isFirstNameValid = validator.checkFirstname("Rekha");
+		Assert.assertTrue(isFirstNameValid);
 	}
 
 	// Test Case for FirstName return False
 	@Test
 	public void checkingFirstNameInvalidReturnFalse() {
 		UserValidator validator = new UserValidator();
-		boolean isFirstName = validator.checkFirstname("riya");
-		Assert.assertFalse(isFirstName);
+		boolean isFirstNameInvalid = validator.checkFirstname("riya");
+		Assert.assertFalse(isFirstNameInvalid);
+	}
+
+	// Test Case for LastName return True
+	@Test
+	public void checkingLastName() {
+		UserValidator user = new UserValidator();
+		boolean isLastNameValid = user.checkLastName("Jambhulkar");
+		Assert.assertTrue(isLastNameValid);
+	}
+
+	// Test Case for LastName return False
+	@Test
+	public void checkingLastNameInvalidMustReturnFalse() {
+		UserValidator user = new UserValidator();
+		boolean isLastNameInvalid = user.checkLastName("jambhulkar");
+		Assert.assertFalse(isLastNameInvalid);
 	}
 }
--- UserValidator.java
+++ UserValidator.java
@@ -5,5 +5,11 @@ public class UserValidator {
 	// Function for Checking the FirstName
 	public boolean checkFirstname(String fName) {
 		return (fName.matches("^[A-Z][a-z]{3,}"));
+
+	}
+
+	// Function for Checking the LastName
+	public boolean checkLastName(String Lname) {
+		return (Lname.matches("^[A-Z][a-z]{3,}"));
 	}
 }
